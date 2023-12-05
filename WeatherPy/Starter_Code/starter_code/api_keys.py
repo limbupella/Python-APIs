9f74ed3f1302e0a99584b4974258d9e5136b6415
limbupella <148026753+limbupella@users.noreply.github.com>
2023-12-04 19:09:47 -0500

    Files

--- WeatherPy/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "b18da20c0c3a9f78e34e151a89ff96e5"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5be436a43cb645d0b7415e9b360a8360"
